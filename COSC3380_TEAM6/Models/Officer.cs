5538e23e4dd86875487b19523c9fc4b950a12cdf
Victor Perez <vperez1104@yahoo.com>
2014-04-03 18:23:54 -0500

    Officer fixed

--- COSC3380_TEAM6/Models/Officer.cs
+++ COSC3380_TEAM6/Models/Officer.cs
@@ -7,32 +7,32 @@ using System.ComponentModel.DataAnnotations;
 namespace COSC3380_TEAM6.Models
 {
     public class Officer
-    {
+    {   [StringLength(6)]
         public int badge_number { get; set; }
-        [StringLength(6)]
-        public int rank { get; set; }
         [StringLength(1)]
+        public int rank { get; set; }
+        [StringLength(30)]
         public string first_name { get; set; }
         [StringLength(30)]
         public string last_name { get; set; }
-        [StringLength(30)]
-        public string user_name { get; set; }
         [StringLength(15)]
-        public string paswerd { get; set; }
+        public string user_name { get; set; }
         [StringLength(20)]
-        public int phone_number { get; set; }
+        public string paswerd { get; set; }
         [StringLength(10)]
-        public string email { get; set; }
+        public int phone_number { get; set; }
         [StringLength(50)]
-        public int ssn { get; set; }
+        public string email { get; set; }
         [StringLength(12)]
-        public int eye_color { get; set; }
+        public int ssn { get; set; }
         [StringLength(1)]
+        public int eye_color { get; set; }
+        [StringLength(2)]
         public int feet { get; set; }
         [StringLength(2)]
         public int inches { get; set; }
-        [StringLength(2)]
-        public int gender { get; set; }
         [StringLength(1)]
+        public int gender { get; set; }
+        
     }
 }
