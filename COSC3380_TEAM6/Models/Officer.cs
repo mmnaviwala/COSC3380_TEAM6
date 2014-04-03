606ec0926e1671278e344712c1c3169b1f520bbe
Victor Perez <vperez1104@yahoo.com>
2014-04-03 00:00:48 -0500

    Officer Table

---
+++ COSC3380_TEAM6/Models/Officer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COSC3380_TEAM6.Models
+{
+    public class Officer
+    {
+        public int badge_number { get; set; }
+        public int rank { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string user_name { get; set; }
+        public string paswerd { get; set; }
+        public int phone_number { get; set; }
+        public string email { get; set; }
+        public int ssn { get; set; }
+        public int eye_color { get; set; }
+        public int feet { get; set; }
+        public int inches { get; set; }
+        public int gender { get; set; }
+    }
+}
