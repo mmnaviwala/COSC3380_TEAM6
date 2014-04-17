bd0958256ae8abda5ea9a6387c9c087918007616
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-04-17 18:10:32 -0500

    Modified the sign in page and _layout slightly

--- Team6/Team6/Models/Officer.cs
+++ Team6/Team6/Models/Officer.cs
@@ -53,6 +53,7 @@ namespace Team6.Models
         public string UserName { get; set; }
 
         [Display(Name = "Password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Display(Name = "Phone Number")]
